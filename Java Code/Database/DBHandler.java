3d50ac83e35e6e7b2a1f51f976a22c01788fe4b0
abdul-manan02 <122822981+abdul-manan02@users.noreply.github.com>
2023-04-28 16:01:22 +0500

    Added explanations for connecting to MySQL Workbench

--- Java Code/Database/DBHandler.java
+++ Java Code/Database/DBHandler.java
@@ -11,6 +11,8 @@ public class DBHandler {
     public DBHandler()
     {
         try {
+            // set these values according the credentials you are using in your MySQL Workbench database //
+            // refer to "https://www.youtube.com/watch?v=e8g9eNnFpHQ" 6:30 onwards if you are still confused //
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/PMS", "root", "12345678");
 //            System.out.println("Connection made to DB");
         } catch (SQLException e) {
